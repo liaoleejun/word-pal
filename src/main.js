0a77e6561686637847f81ee606ee877f88b1ef6a
Leejun Liao <liaoleejun@163.com>
2019-11-27 15:02:44 +0800

    修正radio的value的错误, 并添加getRadioValueByName

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 // 百度统计
 // Google Analytics
 document.getElementById("start").addEventListener("click", function(){
-    alert("Hello World!");
+    let level = getRadioValueByName("level");
+    for (let i = 0; i < data.length; i++) {
+
+    }
 });
+
+// https://stackoverflow.com/questions/9618504/how-to-get-the-selected-radio-button-s-value
+function getRadioValueByName(name) {
+    let radios = document.getElementsByName(name);
+
+    for (let i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            return(radios[i].value);
+        }
+    }
+}

529bebf894a0fc0a8ced8955e74a2d6d919106b8
Leejun Liao <liaoleejun@163.com>
2019-11-28 15:05:57 +0800

    更新设计思路: 英语名词动词的各种语法形态

---
+++ src/HaiBuRuYongRenJiaDe.js
@@ -0,0 +1,34 @@
+// function show_not_in_list() {
+//     let notInList = document.getElementsByClassName("notinlist");
+//
+//     let res = [];
+//     for (let i = 0; i < notInList.length; i++) {
+//         res[i] = notInList[i].innerHTML;
+//     }
+//     res = Array.from(new Set(res)).sort();
+//     for (let item of res) {
+//         console.log(item);
+//     }
+//
+//
+//     let res_format = '';
+//     for (let i = 0; i < res.length; i++) {
+//         res_format += res[i] + "\t\t\t\t";
+//         if (i % 4 === 3) {
+//             res_format += "\n";
+//         }
+//     }
+//     copy(res_format);
+// }
+//
+
+function showNotInList() {
+    let notInList = document.getElementsByClassName("notinlist");
+
+    let res = [];
+    for (let i = 0; i < notInList.length; i++) {
+        res[i] = notInList[i].innerHTML;
+    }
+    res = Array.from(new Set(res)).sort();
+    copy(res.join("\n"));
+}
